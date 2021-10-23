90116937aeb022a6ffecf1975e6913a7c56d0636
aarzilli <alessandro.arzilli@gmail.com>
2021-10-23 15:14:25 +0200

    regexp running

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"os"
-	"net/http"
-	"fmt"
-	"io/ioutil"
 	"bytes"
-	"os/exec"
-	"strings"
-	"time"
 	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
 	"text/tabwriter"
+	"time"
 )
 
 // Reference:
@@ -35,7 +36,6 @@ func uploadPatch(buildName string, diff []byte) string {
 	req.Header.Add("Origin", TEAMCITY_HOST)
 	resp, err := http.DefaultClient.Do(req)
 	must(err)
-	//fmt.Printf("%#v\n", resp)
 	buf, err := ioutil.ReadAll(resp.Body)
 	must(err)
 	return strings.TrimSpace(string(buf))
@@ -59,10 +59,8 @@ func triggerBuild(buildTypeId, changeId string) {
 	req.Header.Add("Origin", TEAMCITY_HOST)
 	resp, err := http.DefaultClient.Do(req)
 	must(err)
-	//fmt.Printf("%#v\n", resp)
 	buf, err := ioutil.ReadAll(resp.Body)
 	must(err)
-	//fmt.Printf("%s\n", string(buf))
 	bs := decodeBuildStatus(bytes.NewReader(buf))
 	fmt.Printf("%s\n", bs.URL())
 }
@@ -85,11 +83,11 @@ func getdiff() []byte {
 }
 
 type buildStatus struct {
-	Id int
+	Id          int
 	BuildTypeId string
-	State string
-	Status string
-	StatusText string
+	State       string
+	Status      string
+	StatusText  string
 }
 
 func decodeBuildStatus(rd io.Reader) *buildStatus {
@@ -111,10 +109,8 @@ func getBuildStatus(buildId string) {
 	req.Header.Add("Origin", TEAMCITY_HOST)
 	resp, err := http.DefaultClient.Do(req)
 	must(err)
-	//fmt.Printf("%#v\n", resp)
 	buf, err := ioutil.ReadAll(resp.Body)
 	must(err)
-	//fmt.Printf("%s\n", string(buf))
 	bs := decodeBuildStatus(bytes.NewReader(buf))
 	w := tabwriter.NewWriter(os.Stdout, 8, 8, 1, ' ', 0)
 	defer w.Flush()
@@ -125,16 +121,37 @@ func getBuildStatus(buildId string) {
 	fmt.Fprintf(w, "Text:\t%s\n", bs.StatusText)
 }
 
+func getBuildTypes() []string {
+	type buildType struct {
+		Id string
+	}
+
+	type buildTypes struct {
+		BuildType []buildType
+	}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://%s/app/rest/buildTypes", TEAMCITY_HOST), nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", TEAMCITY_TOKEN))
+	req.Header.Add("Accept", "application/json")
+	resp, err := http.DefaultClient.Do(req)
+	must(err)
+	var bts buildTypes
+	must(json.NewDecoder(resp.Body).Decode(&bts))
+	r := make([]string, len(bts.BuildType))
+	for i := range bts.BuildType {
+		r[i] = bts.BuildType[i].Id
+	}
+	return r
+}
+
 func usage() {
 	fmt.Fprintf(os.Stderr, `Usage:
 	
+	teamcityrun <regex>		runs current diff on all build types matched (case insensitive) by regex
+	teamcityrun buildtypes		lists all available build types
+	teamcityrun status <build-id>	shows status of build
 	teamcityrun diff			shows current diff
-	teamcityrun all			runs current diff on all architecture combinations
-	teamcityrun os/arch/ver		runs current diff on the specified platform
-	teamcityrun status build-id	shows status of build
 	
-Example:
-	teamcityrun Windows/amd64/1.17
 `)
 	os.Exit(1)
 }
@@ -143,42 +160,46 @@ func main() {
 	if len(os.Args) < 2 {
 		usage()
 	}
-	
-	
+
 	TEAMCITY_TOKEN = os.Getenv("TEAMCITY_TOKEN")
 	TEAMCITY_HOST = os.Getenv("TEAMCITY_HOST")
-	
+
 	switch os.Args[1] {
 	case "status":
 		getBuildStatus(os.Args[2])
-	
+
+	case "buildtypes":
+		v := getBuildTypes()
+		for _, s := range v {
+			fmt.Printf("%s\n", s)
+		}
+
 	case "diff":
 		diff := getdiff()
 		os.Stdout.Write(diff)
-	case "all":
-		// TODO: implement
+
 	default:
-		v := strings.Split(os.Args[1], "/")
-		if len(v) != 3 {
-			fmt.Fprintf(os.Stderr, "wrong platform format")
-			usage()
+		re := regexp.MustCompile("(?i:" + os.Args[1] + ")")
+		bts := []string{}
+		for _, bt := range getBuildTypes() {
+			if re.MatchString(bt) {
+				bts = append(bts, bt)
+			}
 		}
-		os, arch, gover := v[0], v[1], v[2]
-		gover = strings.ReplaceAll(gover, ".", "_")
-		
+		if len(bts) == 0 {
+			fmt.Fprintf(os.Stderr, "no build types match %s\n", os.Args[1])
+			os.Exit(1)
+		}
+
 		id := uploadPatch(time.Now().Format(time.RFC3339), getdiff())
-		fmt.Printf("Patch uploaded at %s\n", id)
-		id = "948"
-		triggerBuild(fmt.Sprintf("Delve_%s_%s_%s", os, arch, gover), id)
-		
-		// Or Delve_AggregatorBuild
+		fmt.Printf("Patch uploaded as %s\n", id)
+
+		for _, bt := range bts {
+			fmt.Printf("%s ", bt)
+			triggerBuild(bt, id)
+		}
 	}
 
-	
-	
 	//TODO:
-	// - return list of build configurations (app/rest/buildTypes)
-	// - trigger build and return build location
-	// - trigger build for aggregator configurations
 	// - download and clean build log
 }
